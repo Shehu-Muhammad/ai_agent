62afc0f83c274795dd42809457221a6375ff0e2a
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-07-11 17:43:17 -0400

    added system prompt to always return I am a robot text

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     message = ""
+    system_prompt = "I\'M JUST A ROBOT"
     if len(sys.argv) == 1:
         print("No message was provided")
         sys.exit(1)
@@ -16,13 +17,18 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=message)]),
     ]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
-    print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
+
     verbose = '--verbose' in sys.argv
     if verbose:
         print(f"User prompt: {message}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(system_prompt)
 
 if __name__ == "__main__":
     main()
