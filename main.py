c8033e70b0af71fcfe2c6cfa0e6f355c934115cb
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-07-12 21:48:20 -0400

    added function declaration for schema_get_files_info

--- main.py
+++ main.py
@@ -2,13 +2,22 @@ import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     message = ""
-    system_prompt = "I\'M JUST A ROBOT"
+    system_prompt = system_prompt = """
+        You are a helpful AI coding agent.
+
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+        - List files and directories
+
+        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
     if len(sys.argv) == 1:
         print("No message was provided")
         sys.exit(1)
@@ -17,18 +26,28 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=message)]),
     ]
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
+
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(response.text)
 
     verbose = '--verbose' in sys.argv
     if verbose:
         print(f"User prompt: {message}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(system_prompt)
 
 if __name__ == "__main__":
     main()
