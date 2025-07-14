3abe1bcf5e3e8c85f77a0918127cbbb100e2504e
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-07-14 02:57:13 -0400

    created call_function

--- main.py
+++ main.py
@@ -2,12 +2,57 @@ import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.get_write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python import schema_run_python_file, run_python_file
+from functions.get_write_file import schema_write_file, write_file
+
+function_map = {
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+    "run_python_file": run_python_file,
+    "write_file": write_file
+}
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    if function_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    else:
+        # Step 1: fetch the function
+        func = function_map[function_name]
+
+        # Step 2: make a copy of the args and add the directory
+        args = dict(function_call_part.args)
+        args["working_directory"] = "./calculator"
+
+        # Step 3: call it with keyword arguments
+        function_result = func(**args)
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
 
 def main():
+    verbose = '--verbose' in sys.argv
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -45,11 +90,22 @@ def main():
 
     if response.function_calls:
         for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            function_call_result = call_function(function_call_part, verbose = verbose)
+
+            # Error check: Is the function response present?
+            if not (
+                hasattr(function_call_result.parts[0], "function_response")
+                and hasattr(function_call_result.parts[0].function_response, "response")
+            ):
+                raise Exception("No function response found!")
+
+            # Only print response if verbose
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+    
     else:
         print(response.text)
 
-    verbose = '--verbose' in sys.argv
     if verbose:
         print(f"User prompt: {message}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
